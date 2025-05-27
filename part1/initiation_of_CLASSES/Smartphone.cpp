897e70e002f680a683257196e0c581fa805e8646
host1l3 <timvitkulevskij@gmail.com>
2025-05-27 21:50:48 +0700

    Update Smartphone.cpp

--- part1/initiation_of_CLASSES/Smartphone.cpp
+++ part1/initiation_of_CLASSES/Smartphone.cpp
@@ -3,21 +3,16 @@
 Smartphone::Smartphone() {
 	model = "";
 	specs = SmSpec{};
-	price.setPrice(0);
-	price.setCurrency(RUB);
 }
 
-Smartphone::Smartphone(string model, double price, SmSpec spec, Mtype currency) {
+Smartphone::Smartphone(string model, SmSpec spec) {
 	this->model = model;
 	this->specs = spec;
-	this->price.setPrice(price);
-	this->price.setCurrency(currency);
 }
 
 Smartphone& Smartphone::operator=(const Smartphone& other) {
 	if (this != &other) {
 		model = other.model;
-		price = other.price;
 		specs = other.specs;
 	}
 	return *this;
@@ -27,22 +22,11 @@ string Smartphone::getModel() const {
 	return model;
 }
 
-Price Smartphone::getPrice() const {
-	return price;
-}
-
 SmSpec Smartphone::getSpec() const {
 	return specs;
 }
 
-void Smartphone::setPrice(double c, Mtype def) {
-	price.setPrice(c);
-	price.setCurrency(def);
-}
-
 void Smartphone::Print() const {
 	cout << model << endl;
-	cout << "Price: " << price.getAmount() << " " << MtypeName[price.getCurrency()] << endl;
 	specs.print();
-	cout << endl;
 }
