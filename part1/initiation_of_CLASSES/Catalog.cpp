d2f729669d221861888928f2627a4518455d95c4
host1l3 <timvitkulevskij@gmail.com>
2025-05-27 21:52:08 +0700

    Update Catalog.cpp

--- part1/initiation_of_CLASSES/Catalog.cpp
+++ part1/initiation_of_CLASSES/Catalog.cpp
@@ -1,24 +1,11 @@
 #include "Catalog.h"
 
-void Catalog::addSmartphone(string model, double price, Manufacturer manufacturer,
-	Color color, float display, short ram,
-	short storage, Manufacturer cpu, Os os, short ncams, Protection protection, Mtype currency) {
-	smartphones.push_back(Smartphone{ model, price, SmSpec{manufacturer, color, 
-		ram, storage, cpu, os, display, ncams, protection}, currency});
-	lenght++;
-}
-
-void Catalog::addSmartphone(string model, double price, SmSpec spec, Mtype currency) {
-	smartphones.push_back(Smartphone{ model, price, spec, currency });
-	lenght++;
-}
-
-void Catalog::addSmartphone(const Smartphone& phone) {
+void Catalog::addSmartphone(const Product& phone) {
 	smartphones.push_back(phone);
 	lenght++;
 }
 
-void Catalog::addSmartphone(Smartphone&& phone) {
+void Catalog::addSmartphone(Product&& phone) {
 	smartphones.push_back(std::move(phone));
 	lenght++;
 }
@@ -27,127 +14,137 @@ int Catalog::Length() {
 	return lenght;
 }
 
-Smartphone Catalog::getSmartphone(string model) {
-	for (Smartphone s : smartphones) {
-		if (s.getModel() == model)  return s;
-	}  return Smartphone();
+Product Catalog::getSmartphone(string model) {
+	for (Product s : smartphones) {
+		if (s.getSmartphone().getModel() == model)  return s;
+	}  
+	static Smartphone s{};
+	Product p{ s };
+	return p;
 }
 
-Smartphone Catalog::search(const Smartphone& searchSmartphone) {
+Product Catalog::search(const Smartphone& searchSmartphone) {
 	int bestScore = -1;
 	Smartphone bestCandidate;
 
-	for (const Smartphone& s : smartphones) {
+	for (const Product& s : smartphones) {
 		int score = 0;
 
 		{
-			if (!searchSmartphone.getModel().empty() && searchSmartphone.getModel() == s.getModel())
+			if (!searchSmartphone.getModel().empty() && searchSmartphone.getModel() == s.getSmartphone().getModel())
 				score++;
+			else continue;
 
 			if (!ManufacturerNames[searchSmartphone.getSpec().getManufacturer()].empty() && 
-				searchSmartphone.getSpec().getManufacturer() == s.getSpec().getManufacturer())
+				searchSmartphone.getSpec().getManufacturer() == s.getSmartphone().getSpec().getManufacturer())
 				score++;
 
 			if (!ColorNames[searchSmartphone.getSpec().getColor()].empty() && 
-				searchSmartphone.getSpec().getColor() == s.getSpec().getColor())
+				searchSmartphone.getSpec().getColor() == s.getSmartphone().getSpec().getColor())
 				score++;
 
-			if (searchSmartphone.getSpec().getDisplay() == s.getSpec().getDisplay())
+			if (searchSmartphone.getSpec().getDisplay() == s.getSmartphone().getSpec().getDisplay())
 				score++;
 
-			if (searchSmartphone.getSpec().getRAM() == s.getSpec().getRAM())
+			if (searchSmartphone.getSpec().getRAM() == s.getSmartphone().getSpec().getRAM())
 				score++;
 
-			if (searchSmartphone.getSpec().getStorage() == s.getSpec().getStorage())
+			if (searchSmartphone.getSpec().getStorage() == s.getSmartphone().getSpec().getStorage())
 				score++;
 
 			if (!ManufacturerNames[searchSmartphone.getSpec().getCPU()].empty() &&
-				searchSmartphone.getSpec().getCPU() == s.getSpec().getCPU())
+				searchSmartphone.getSpec().getCPU() == s.getSmartphone().getSpec().getCPU())
 				score++;
 
 			if (!OsNames[searchSmartphone.getSpec().getOS()].empty() && 
-				searchSmartphone.getSpec().getOS() == s.getSpec().getOS())
+				searchSmartphone.getSpec().getOS() == s.getSmartphone().getSpec().getOS())
 				score++;
 
 			if (!ProtHave[searchSmartphone.getSpec().getProtection()].empty() && 
-				searchSmartphone.getSpec().getProtection() == s.getSpec().getProtection())
+				searchSmartphone.getSpec().getProtection() == s.getSmartphone().getSpec().getProtection())
 				score++;
 
-			if (searchSmartphone.getSpec().getNCams() == s.getSpec().getNCams())
+			if (searchSmartphone.getSpec().getNCams() == s.getSmartphone().getSpec().getNCams())
 				score++;
 		}
 
-		if (score > bestScore && score > 3) {
+		if (score > 0) {
 			bestScore = score;
-			bestCandidate = s;
+			bestCandidate = s.getSmartphone();
 		}
 	}
-
+	Product BC{bestCandidate};
 	if (!bestCandidate.getModel().empty())
-		return bestCandidate;
-	else return Smartphone{};
+		return BC;
+	else {
+		static Smartphone s{};
+		Product p{ s };
+		return p;
+	}
 }
 
-Smartphone Catalog::search(string model, Manufacturer manufacturer) {
+Product Catalog::search(string model, Manufacturer manufacturer) {
 	int bestScore = -1;
 	Smartphone bestCandidate;
-	for (const Smartphone& s : smartphones) {
+	for (const Product& s : smartphones) {
 		int score = 0;
 
 		{
-			if (!model.empty() && model == s.getModel()) score++;
+			if (!model.empty() && model == s.getSmartphone().getModel()) score++;
 
-			if (manufacturer == s.getSpec().getManufacturer()) score++;
+			if (manufacturer == s.getSmartphone().getSpec().getManufacturer()) score++;
 		}
 
 		if (score > bestScore) {
 			bestScore = score;
-			bestCandidate = s;
+			bestCandidate = s.getSmartphone();
 		}
 	}
-	return bestCandidate;
+	Product BC{bestCandidate};
+	return BC;
 }
 
-vector<Smartphone> Catalog::search(Smartphone& searchSmartphone) {
-	vector<Smartphone> results;
+vector<Product> Catalog::search(Smartphone& searchSmartphone) {
+	vector<Product> results;
 	int bestScore = -1;
 	Smartphone bestCandidate;
 
-	for (Smartphone s : smartphones) {
+	for (Product s : smartphones) {
 		int score = 0;
 
 		string model = searchSmartphone.getModel();
 		transform(model.begin(), model.end(), model.begin(), tolower);
-		string model2 = s.getModel();
+		string model2 = s.getSmartphone().getModel();
 		transform(model2.begin(), model2.end(), model2.begin(), tolower);
 		if (model == model2) score++;
+		else continue;
 
 		Manufacturer manufacturer = searchSmartphone.getSpec().getManufacturer();
-		if (manufacturer != Manufacturer::UNDEFM && manufacturer != s.getSpec().getManufacturer()) 
+		if (manufacturer != Manufacturer::UNDEFM && manufacturer != s.getSmartphone().getSpec().getManufacturer())
 			score++;
 
 		Color color = searchSmartphone.getSpec().getColor();
-		if (color != Color::UNDEFC && color == s.getSpec().getColor())
+		if (color != Color::UNDEFC && color == s.getSmartphone().getSpec().getColor())
 			score++;
 
 		float displaySize = searchSmartphone.getSpec().getDisplay();
-		if (displaySize > 0 && displaySize == s.getSpec().getDisplay())
+		if (displaySize > 0 && displaySize == s.getSmartphone().getSpec().getDisplay())
 			score++;
 
 		int ram = searchSmartphone.getSpec().getRAM();
-		if (ram > 0 && ram != s.getSpec().getRAM()) 
+		if (ram > 0 && ram != s.getSmartphone().getSpec().getRAM())
 			score++;
 
 		int storage = searchSmartphone.getSpec().getStorage();
-		if (storage > 0 && storage != s.getSpec().getStorage()) 
+		if (storage > 0 && storage != s.getSmartphone().getSpec().getStorage())
 			score++;
 
 		Manufacturer cpu = searchSmartphone.getSpec().getCPU();
-		if (cpu != Manufacturer::UNDEFM && cpu != s.getSpec().getCPU()) 
+		if (cpu != Manufacturer::UNDEFM && cpu != s.getSmartphone().getSpec().getCPU())
 			score++;
 
 		Os os = searchSmartphone.getSpec().getOS();
-		if (os != Os::UNDEFO && os == s.getSpec().getOS()) 
+		if (os != Os::UNDEFO && os == s.getSmartphone().getSpec().getOS())
 			score++;
 
 		if (score > 0)
@@ -156,5 +153,6 @@ vector<Smartphone> Catalog::search(Smartphone& searchSmartphone) {
 			bestScore = score;
 		}
 	}
+
 	return results;
 }
